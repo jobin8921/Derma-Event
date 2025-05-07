8a11a5447fab549c3564caaf4cf9c44ad2f1ff6e
JeswinDanJohnson <jeswinkoloor@gmail.com>
2025-05-07 13:34:15 +0530

    no message

--- registration/urls.py
+++ registration/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('schedule/', views.about, name='schedule'),
+    path('speakers/', views.about, name='speakers'),
+    path('blog/', views.about, name='blog'),
+    path('contact/', views.about, name='contact'),
     path('registration_success/', views.registration_success, name='registration_success'),  # Success page
     path('admin-panel/', views.admin_panel, name='admin_panel'),
     path('logout/', views.logout_view, name='logout'),
